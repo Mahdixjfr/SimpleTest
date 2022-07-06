c9ebbf4b43c86b0c7f781a3702a03e62caedddb9
Mahdixjfr <mahdi.seri333@gmail.com>
2022-07-06 10:11:51 -0700

    add prices attribute

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,4 +16,19 @@ class Product extends Model
         'inventory',
         'description'
     ];
+
+    public function getPricesAttribute($value)
+    {
+        if ($value > 999 && $value < 999999) {
+            $number = $value / 1000;
+            $number = number_format($number);
+            return $number . ' هزار';
+        } elseif ($value > 999999) {
+            $number = $value / 1000000;
+            $number = number_format($number);
+            return $number . ' میلیون';
+        } else {
+            return $value;
+        }
+    }
 }
