e6c4cfc3583e4316a6ee21df9b3ee1e41479b7e7
Mahdixjfr <mahdi.seri333@gmail.com>
2022-07-06 11:28:36 +0430

    add function typePrice to helpers function

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@
 
 use App\Models\User;
 
-if (!function_exists('show_name')) {
+if (!function_exists('showName')) {
     function show_name($id)
     {
         $user = User::find($id);
@@ -11,13 +11,30 @@ if (!function_exists('show_name')) {
     }
 }
 
-if (!function_exists('UserId')) {
-    function UserId()
+if (!function_exists('userId')) {
+    function userId()
     {
         if (auth()->check()) {
             return auth()->user()->id;
         } else {
             return redirect('/login');
+        }
+    }
+}
+
+if (!function_exists('typePrice')) {
+    function typePrice($value)
+    {
+        if ($value > 999 && $value < 999999) {
+            // $number = $value / 1000;
+            $number = number_format($value);
+            return   ' هزار' . $number;
+        } elseif ($value > 999999) {
+            // $number = $value / 1000000;
+            $number = number_format($value);
+            return ' میلیون' . $number;
+        } else {
+            return $value;
         }
     }
 }
