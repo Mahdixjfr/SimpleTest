1dbe3de6f60ada6225e61c42b86c016516433819
Mahdixjfr <mahdi.seri333@gmail.com>
2022-07-11 14:01:23 +0430

    add delivered model

---
+++ app/Http/Controllers/DeliveredController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Delivered;
+use Illuminate\Http\Request;
+
+class DeliveredController extends Controller
+{
+    public function delivered()
+    {
+        $delivereds = [];
+        if ($this->checkDElivered()) {
+            $last_id_delivered = Delivered::where('user_id', userId())->orderBy('order', 'desc')->first()->order;
+
+            for ($i = $last_id_delivered; $i >= 1; $i--) {
+                $objects = Delivered::where('user_id', userId())->where('order', $i)->get();
+                foreach ($objects as $delivered) {
+                    $number = $delivered->number;
+                    $price = unformatNumber($delivered->product()->first()->price);
+                    $this->total = $this->total + $number * $price;
+                }
+                $delivereds[] = [
+                    'products' => $objects,
+                    'total' => number_format($this->total)
+                ];
+            }
+        }
+        return view('delivered', compact('delivereds'));
+    }
+
+    public function checkDElivered()
+    {
+        $result = Delivered::where('user_id', userId())->orderBy('order', 'desc');
+        if ($result->count() > 0) {
+            return [$result, true];
+        } else {
+            return false;
+        }
+    }
+}
