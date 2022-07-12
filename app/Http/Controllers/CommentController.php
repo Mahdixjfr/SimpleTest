1312e24ef654956144b603dbd91ac315b2220306
Mahdixjfr <mahdi.seri333@gmail.com>
2022-07-12 12:39:07 +0430

    create functions for like&dislike comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CommentRequest;
 use App\Models\Comment;
+use App\Models\DisLikeComment;
+use App\Models\LikeComment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -16,5 +18,50 @@ class CommentController extends Controller
             'product_id' => $product_id,
         ]);
         return back();
+    }
+
+    public function like(Comment $comment)
+    {
+        $this->functor($comment, LikeComment::class, 'like');
+        return back();
+    }
+    public function dislike(Comment $comment)
+    {
+        $this->functor($comment, DisLikeComment::class, 'dislike');
+        return back();
+    }
+
+    public function check($user_id, $comment_id, $model)
+    {
+        $result = $model::where('user_id', $user_id)->where('comment_id', $comment_id);
+        if ($result->count() > 0) {
+            return $result->first();
+        }
+        return true;
+    }
+
+    public function addOperator($comment_id, $model, $comment, $type)
+    {
+        $model::create([
+            'user_id' => userId(),
+            'comment_id' => $comment_id,
+        ]);
+        $comment->update([$type => $comment->{$type} + 1]);
+    }
+
+    public function deleteOpeator($obj, $comment, $type)
+    {
+        $obj->delete();
+        $comment->update([$type => $comment->{$type} - 1]);
+    }
+
+    public function functor($comment, $model, $operator)
+    {
+        if ($this->check(userId(), $comment->id, $model) === true) {
+            $this->addOperator($comment->id, $model, $comment, $operator);
+        } else {
+            $obj = $this->check(userId(), $comment->id, $model);
+            $this->deleteOpeator($obj, $comment, $operator);
+        }
     }
 }
