a64b4d478445f025495f57df21f0b4b51700b096
Mahdixjfr <mahdi.seri333@gmail.com>
2022-07-27 14:52:11 +0430

    add relationship in seller model

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -10,4 +10,9 @@ class Seller extends Model
     use HasFactory;
 
     public $fillable = ['user_id', 'number', 'address', 'store_name', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
