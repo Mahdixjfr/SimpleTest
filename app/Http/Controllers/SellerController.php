5ed7a3ea3de9b92f5c6863832cd69f98c901f9ed
Mahdixjfr <mahdi.seri333@gmail.com>
2022-08-06 14:39:01 +0430

    add fucntion  create products & delete

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductRequest;
 use App\Http\Requests\SellerRegisterRequest;
+use App\Models\Product;
 use App\Models\Seller;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth.seller')->except('showRegisterationForm');
+    }
+    public function findCategorySeller()
+    {
+        $category = Seller::find(userId())->category()->first();
+        return $category;
+    }
+
     public function showRegisterationForm()
     {
         return view('Seller/register');
@@ -41,13 +54,36 @@ class SellerController extends Controller
 
     public function store()
     {
-        return view('Seller/create');
+        $category_name = $this->findCategorySeller()->name;
+        return view('Seller/create', compact('category_name'));
     }
 
     public function create(ProductRequest $request)
     {
         $file = $request->photo;
-        $result = $file->move(public_path('/storage/photo/'), $file->getClientOriginalName());
-        dd($result);
+        $photo = rand(10000, 100000) . $file->getClientOriginalName();
+        $result = $file->move(public_path('\storage\photo'), $photo);
+        $category_id = $this->findCategorySeller()->id;
+        $validated_data = $request->all();
+        $validated_data['photo'] = $photo;
+        $product = [
+            'category_id' => $category_id,
+            'seller_id' => userId(),
+        ];
+        $product = array_merge($validated_data, $product);
+        Product::create($product);
+        return redirect('/');
+    }
+
+    public function products()
+    {
+        $products = Seller::find(userId())->products()->paginate(15);
+        return view('Seller/products', compact('products'));
+    }
+
+    public function deleteProduct(Product $product)
+    {
+        $product->delete();
+        return back();
     }
 }
