4a0f82e7bf4a1178a60ae424994eabaf1c69dbb7
Mahdixjfr <mahdi.seri333@gmail.com>
2022-08-06 14:37:54 +0430

    orderby home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     public function index()
     {
         return view('home', [
-            'products' => Product::paginate(15),
+            'products' => Product::orderBy('id', 'desc')->paginate(15),
         ]);
     }
 
