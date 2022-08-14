d95f50c47d994d6e25cb62f30cfb07509930d1f1
Mahdixjfr <mahdi.seri333@gmail.com>
2022-08-14 14:39:22 +0430

    change extend file ro index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.index')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
+@section('main')
+<div class="main">
+    <!-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -42,6 +42,8 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
+
+
 </div>
 @endsection
