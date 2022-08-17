f3f00735aa20256fcd137651792ef3003889c276
Mahdixjfr <mahdi.seri333@gmail.com>
2022-08-17 15:03:16 +0430

    styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,30 +2,46 @@
 
 @section('main')
 <div class="main">
+    <div class="bg-one">
+        <a href=""><img class="bg-image" src="backgrounds/tablet.jpg" alt=""></a>
+        <a href=""><img class="bg-image" src="backgrounds/headphone.jpg" alt=""></a>
+    </div>
     <h2 class="title">جدیدترین ها</h2>
     <div class="owl-carousel owl-theme carousel">
         @foreach($newProducts as $product)
         <div class="item">
-            <a class="product-link" href="/product/{{$product->id}}"><img src="storage/photo/{{$product->photo}}" alt=""></a>
+            <a class="product-link" href="/product/{{$product->id}}"><img class="carousel-image" src="storage/photo/{{$product->photo}}" alt=""></a>
             <div class="carousel-name">{{$product->name}}</div>
             <div class="carousel-price">{{$product->price}} <span class="ltr">تومان</span></div>
         </div>
         @endforeach
+    </div>
+    <div class="bg-two">
+        <a href=""><img class="bg-image" src="backgrounds/laptopbg.jpg" alt=""></a>
+        <a href=""><img class="bg-image" src="backgrounds/phonebg.jpg" alt=""></a>
+        <a href=""><img class="bg-image" src="backgrounds/phonebg1.jpg" alt=""></a>
+        <a href=""><img class="bg-image" src="backgrounds/watchbg.jpg" alt=""></a>
     </div>
     <h2 class="title">پر بازدیدترین ها</h2>
     <div class="owl-carousel owl-theme carousel">
         @foreach($newProducts as $product)
         <div class="item">
-            <a class="product-link" href="/product/{{$product->id}}"><img src="storage/photo/{{$product->photo}}" alt=""></a>
+            <a class="product-link" href="/product/{{$product->id}}"><img class="carousel-image" src="storage/photo/{{$product->photo}}" alt=""></a>
             <div class="carousel-name">{{$product->name}}</div>
             <div class="carousel-price">{{$product->price}} <span class="ltr">تومان</span></div>
         </div>
         @endforeach
     </div>
+
 </div>
 
 
-
+<style>
+    .owl-item {
+        display: flex;
+        justify-content: center;
+    }
+</style>
 
 
 
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,12 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="js/owlcarousel/dist/assets/owl.carousel.min.css">
     <link rel="stylesheet" href="js/owlcarousel/dist/assets/owl.theme.default.min.css">
+    <script src="{{ asset('js/bootstrap.js') }}" defer></script>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('js/bootstrap.js') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="js/owlcarousel/dist/owl.carousel.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/75a773a8a3.js" crossorigin="anonymous"></script>
 
     <title>@yield('title')</title>
 </head>
@@ -20,35 +24,86 @@
 <body>
     <div class="contain">
         <div class="header">
-            <header>
-                <div class="title">
-                    <h1 class="header-title">IRAN <span class="star">STAR</span></h1>
-                </div>
-                <div class="search">
-                    <input type="text" name="search" class="input-search">
-                </div>
-                <div class="sign">
-                    <a href="/login">ورود | ثبت نام</a>
-                </div>
-            </header>
+            <div class="landing-wrapper">
+                <header>
+                    <div class="title">
+                        <h1 class="header-title">IRAN <span class="star">STAR</span></h1>
+                    </div>
+                    <div class="search">
+                        <input type="text" name="search" class="input-search">
+                    </div>
+                    @guest
+                    @if (Route::has('login'))
+                    <div class="sign">
+                        <a href="/login">ورود | ثبت نام</a>
+                    </div>
+                    @endif
+                    @else
+                    <i id="user" class="fa-solid fa-circle-user fa-2x"></i>
+                </header>
+            </div>
             <ul class="nav-bar">
+                <!-- Authentication Links -->
+                <li class="dropdown show">
+                    <a class=" nav-item" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        دسته بندی کالا ها
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <ul class="dropdown-list">
+                            @foreach($categories as $category)
+                            <li class="ddown-item"><a class="ddown-link" href="{{ route('category' , ['category' => $category->id]) }}">{{$category->name}}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </li>
+                <li class="dropdown show">
+                    <a class=" nav-item dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <ul class="dropdown-list">
+                            <li class="ddown-item"><a class="ddown-link" href="{{ route('profile') }}">پنل کاربری</a></li>
+                            <li class="ddown-item"><a class="ddown-link" href="/cart">سبد خرید</a></li>
+                            <li class="ddown-item"><a class="ddown-link" href="{{ route('delivered') }}">تحویل شده</a></li>
+                            <li class="ddown-item"><a class="ddown-link" href="{{ route('favorites') }}">علاقه مندی ها</a></li>
+                            <li class="ddown-item"><a class="ddown-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    خروج از حساب کاربری
+                                </a></li>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </ul>
+
+                    </div>
+                </li>
+
+                @if(! checkSeller())
+                <li class="">
+                    <a class="nav-item" href="{{ route('showRegisterSeller') }}">فروشنده شو</a>
+                </li>
+                @endif
+                @endguest
+
+            </ul>
+            <!-- <ul class="nav-bar">
                 <li class="nav-item">link 1</li>
                 <li class="nav-item">link 2</li>
                 <li class="nav-item">link 3</li>
                 <li class="nav-item">link 4</li>
-            </ul>
+            </ul> -->
         </div>
         @yield('main')
         <div class="footer">
             <footer></footer>
         </div>
     </div>
-    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script> -->
-
-
 
 
 
