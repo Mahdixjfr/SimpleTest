421446566f5d842fb4d840343c549f2b41836ad0
Mahdixjfr <mahdi.seri333@gmail.com>
2022-09-26 20:58:04 -0700

    add field phone & address to users

--- database/migrations/2022_09_24_203056_add_field_number_address_to_users.php
+++ database/migrations/2022_09_24_203056_add_field_number_address_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('number');
+            $table->string('phone');
             $table->text('address')->nullable();
         });
     }
@@ -27,7 +27,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
         });
     }
 };
