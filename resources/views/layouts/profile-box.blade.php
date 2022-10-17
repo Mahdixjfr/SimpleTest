4619bdfaef7032ea6ddbefef6ef5bfc6e25c0a79
Mahdixjfr <mahdi.seri333@gmail.com>
2022-10-17 12:31:43 -0700

    fontawesome

--- resources/views/layouts/profile-box.blade.php
+++ resources/views/layouts/profile-box.blade.php
@@ -17,7 +17,10 @@
     <script src="/js/owlcarousel/dist/owl.carousel.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/75a773a8a3.js" crossorigin="anonymous"></script>
+    <!-- <script src="https://kit.fontawesome.com/75a773a8a3.js" crossorigin="anonymous"></script> -->
+    <link href="/fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="/fontawesome/css/brands.css" rel="stylesheet">
+    <link href="/fontawesome/css/solid.css" rel="stylesheet">
 
     <title>ایران استار</title>
 </head>
